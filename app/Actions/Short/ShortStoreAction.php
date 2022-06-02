4df744923d40794e5c454783ba54aad421368b4e
Luan Ramos <luan.ramos@kolaresti.com.br>
2022-06-02 00:48:02 -0300

    implementando modulo de shorts

---
+++ app/Actions/Short/ShortStoreAction.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Actions\Short;
+
+class ShortStoreAction
+{
+    function __construct()
+    {
+
+    }
+}
---
+++ app/Actions/Short/ShortUpdateAction.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Actions\Short;
+
+class ShortUpdateAction
+{
+    function __construct()
+    {
+
+    }
+}
